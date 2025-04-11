66feb8b306f591dac7aec713c7523576817b94b3
Paul Repin <paul.repin@gmail.com>
2025-04-11 16:56:56 +0300

    Выносит секрет и настройки токенов доступа в конфиг

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -71,7 +71,7 @@ func initializeApp() error {
 
 	productService := product.NewProductService(productRepository)
 	offerService := offer.NewOfferService(offerRepository)
-	tokenService := token.NewTokenService(tokenRepository, "secret", 30*24*time.Hour, 1*time.Hour)
+	tokenService := token.NewTokenService(tokenRepository, cfg.Token.Secret, cfg.Token.AccessTokenDuration, cfg.Token.RefreshTokenDuration)
 	userService := user.NewUserService(userRepository, tokenService)
 	notificationService := notification.NewNotificationService(notificationRepository)
 
