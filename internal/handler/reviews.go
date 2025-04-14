9cb1e0b16235705a7e835aee7409081f6a341a97
Vladislav Khorunzhiy <v.m.khorunzhiy@yandex.ru>
2025-04-14 21:13:28 +0300

    add zap logger and product review handler

---
+++ internal/handler/reviews.go
@@ -0,0 +1,82 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zuzaaa-dev/stawberry/internal/domain/entity"
+	"github.com/zuzaaa-dev/stawberry/internal/handler/dto"
+	"go.uber.org/zap"
+)
+
+type ProductReviewService interface {
+	AddRewiew(ctx context.Context, productID int, userID int, rating int, review string) error
+	GetReviewsByProductId(ctx context.Context, productID int) ([]entity.ProductReview, error)
+}
+
+type productReviewHandler struct {
+	prs    ProductReviewService
+	logger *zap.Logger
+}
+
+func NewProductReviewHandler(prs ProductReviewService) productReviewHandler {
+	return productReviewHandler{prs: prs}
+}
+
+func (prh *productReviewHandler) AddReview(c *gin.Context) {
+	const op = "productReviewHandler.AddReviews()"
+	log := prh.logger.With(zap.String("op", op))
+
+	productID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid productID"})
+		log.Warn("Failed to parse productID", zap.Error(err))
+		return
+	}
+
+	var addReview = dto.AddReview
+	if err := c.ShouldBindJSON(&addReview); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+		log.Warn("Failed to bind JSON", zap.Error(err))
+		return
+	}
+
+	userID, ok := c.Get("userID")
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "user not authenticated"})
+		log.Warn("Failed to get userID from context", zap.Error(err))
+		return
+	}
+
+	err = prh.prs.AddRewiew(c.Request.Context(), productID, userID.(int), addReview.Rating, addReview.Review)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to add review"})
+		log.Warn("Failed to add review", zap.Error(err))
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Review added successfully"})
+}
+
+func (prh *productReviewHandler) GetReviews(c *gin.Context) {
+	const op = "productReviewHandler.GetReviews()"
+	log := prh.logger.With(zap.String("op", op))
+
+	productID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid productID"})
+		log.Warn("Failed to parse productID", zap.Error(err))
+		return
+	}
+
+	reviews, err := prh.prs.GetReviewsByProductId(c.Request.Context(), productID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to fetch reviews"})
+		log.Warn("failed to fetch reviews", zap.Error(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, reviews)
+}
