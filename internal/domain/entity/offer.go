6a293b9c79e341f05cc999c111b61fe1ed909658
Lawitz2 <eldrasseil@gmail.com>
2025-05-16 09:45:58 +0300

    tags, conversion funcs/methods, remove unnecessary fields

--- internal/domain/entity/offer.go
+++ internal/domain/entity/offer.go
@@ -3,13 +3,14 @@ package entity
 import "time"
 
 type Offer struct {
-	ID        uint      `json:"id" db:"id"`
-	UserID    uint      `json:"user_id" db:"user_id"`
-	ProductID uint      `json:"product_id" db:"product_id"`
-	ShopID    uint      `json:"store_id" db:"shop_id"`
-	Price     float64   `json:"price" db:"offer_price"`
-	Status    string    `json:"status" db:"status"`
-	ExpiresAt time.Time `json:"expires_at" db:"expires_at"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	ID        uint
+	Price     float64
+	Currency  string
+	Status    string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	ExpiresAt time.Time
+	ShopID    uint
+	UserID    uint
+	ProductID uint
 }
