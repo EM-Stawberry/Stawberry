bf7a0fceb70ef23c7770ee44315e0bb2d573d869
Lawitz2 <eldrasseil@gmail.com>
2025-06-07 01:42:42 +0300

    batch logging

--- internal/repository/audit.go
+++ internal/repository/audit.go
@@ -19,35 +19,44 @@ func NewAuditRepository(db *sqlx.DB) *AuditRepository {
 	return &AuditRepository{db: db}
 }
 
-func (ar *AuditRepository) LogStore(ae entity.AuditEntry) error {
-	insertAuditLogQ, args := squirrel.Insert("audit_logs").
-		Columns(
-			"method",
-			"url",
-			"resp_status",
-			"user_ip",
-			"user_id",
-			"user_role",
-			"received_at",
-			"req_body",
-			"resp_body").
-		Values(
-			ae.Method,
-			ae.Url,
-			ae.RespStatus,
-			ae.IP,
-			ae.UserID,
-			ae.UserRole,
-			ae.ReceivedAt,
-			model.JSONBMap(ae.ReqBody),
-			model.JSONBMap(ae.RespBody)).
-		PlaceholderFormat(squirrel.Dollar).
-		MustSql()
+func (ar *AuditRepository) LogStore(ae []entity.AuditEntry) error {
+	models := model.ConvertEntityToAuditEntries(ae)
+
+	qb := squirrel.Insert("audit_logs").Columns(
+		"method",
+		"url",
+		"resp_status",
+		"user_ip",
+		"user_id",
+		"user_role",
+		"received_at",
+		"req_body",
+		"resp_body",
+	)
+
+	for _, m := range models {
+		qb = qb.Values(
+			m.Method,
+			m.Url,
+			m.RespStatus,
+			m.IP,
+			m.UserID,
+			m.UserRole,
+			m.ReceivedAt,
+			m.ReqBody,
+			m.RespBody,
+		)
+	}
+
+	insertAuditLogQ, args, err := qb.PlaceholderFormat(squirrel.Dollar).ToSql()
+	if err != nil {
+		return err
+	}
 
 	ctxTO, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer ctxCancel()
 
-	_, err := ar.db.ExecContext(ctxTO, insertAuditLogQ, args...)
+	_, err = ar.db.ExecContext(ctxTO, insertAuditLogQ, args...)
 	if err != nil {
 		return err
 	}
